336ad1b04e174afd11ac5930940b2ad5426fd8fa
azeem1234coder <odusanyaabdulazeem25@gmail.com>
2024-09-22 21:38:26 +0100

    iii

---
+++ Html DOM/jjj.js
@@ -0,0 +1,6 @@
+function myfunction(){
+document.getElementById("result").innerHTML=
+document.getElementById("demo").firstChild.nodeValue;
+document.getElementById("result1").innerHTML=
+Document.getElementById("demo").childNodes[0].nodeValue;
+}
